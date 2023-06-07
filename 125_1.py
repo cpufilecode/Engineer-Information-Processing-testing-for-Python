29a645acc59a7dc073e145831f72593b315282da
cpufilecode <zf0908@naver.com>
2023-06-07 22:16:04 +0900

    upload

--- 125_1.py
+++ 125_1.py
@@ -2,6 +2,6 @@ x = input('입력 :')
 a = [ 'abc123','def456', 'ghi789' ]
 a.append(x)
 a.remove('def456')
-print(a[1][-3:], a[2][:-3], sep = '.')
+print(a[1][-3:], a[2][:-3], sep = ',')
 for i in range(3, 6):
     print(i, end = ' ')
